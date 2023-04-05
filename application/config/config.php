966ed6d66b9cf34eb24b337c2c9ff0aee1515274
thepuremilk <farhanputra717@gmail.com>
2023-04-05 21:20:42 +0700

    pertemuan3Template

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'http://localhost/codeigniter3/';
 
 /*
 |--------------------------------------------------------------------------
@@ -89,7 +89,7 @@ $config['language']	= 'english';
 | that require a character set to be provided.
 |
 | See http://php.net/htmlspecialchars for a list of supported charsets.
-|
+
 */
 $config['charset'] = 'UTF-8';
 
