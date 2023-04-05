d9a2f5408fe9f11e97ee39c48ee9b2a9559b8b30
thepuremilk <farhanputra717@gmail.com>
2023-04-05 20:26:34 +0700

    pertemuan3

--- application/controllers/Latihan1.php
+++ application/controllers/Latihan1.php
@@ -9,8 +9,10 @@ class Latihan1 extends CI_Controller
     public function penjumlahan($n1, $n2) 
     { 
         $this->load->model('Model_latihan1');
+
         $data['nilai1'] = $n1; 
         $data['nilai2'] = $n2; 
         $data['hasil'] = $this->Model_latihan1->jumlah($n1, $n2); 
+        
         $this->load->view('view-latihan1', $data); }
 }
